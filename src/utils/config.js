0ddca8d30e4d8600e238efcb66ce8d232866bbf4
dmitrysonder <dmitry.sonder@gmail.com>
2021-05-10 21:53:31 +0200

    DEV env added to config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,11 +1,12 @@
 exports.config = {
+    ENV: "PROD",
     PORT: 3000,
     AWS_REGION: process.env.AWS_REGION || 'us-east-2', // AWS Region
     INFURA_KEY: process.env.INFURA_KEY, // Infura API key for eth testing
     TABLE_NAME: process.env.TABLE_NAME || 'holvis', // DynamoDB name
     BUCKET_NAME: process.env.BUCKET_NAME || 'project001',  // S3 bucket name  
     CHUNK_SIZE: process.env.CHUNK_SIZE || 10, // max simulateonos records in database
-    ADDRESS: process.env.AWS_REGION ? "0.0.0.0" : "127.0.0.1",
+    ADDRESS: this.ENV === "PROD" ? "0.0.0.0" : "127.0.0.1",
     getProvider: function () {
         return ["mainnet", {infura: this.INFURA_KEY}]
     }
