13ca60029cb65ac8ba991d858288733c28582cd1
dmitrysonder <dmitry.sonder@gmail.com>
2021-05-11 21:33:55 +0200

    config updated

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ exports.config = {
     PORT: 3000,
     AWS_REGION: process.env.AWS_REGION || 'us-east-2', // AWS Region
     INFURA_KEY: process.env.INFURA_KEY || '694aa307fb484c5bac98e4dca4aca053', // Infura API key for eth testing
-    TABLE_NAME: process.env.TABLE_NAME || 'holvis', // DynamoDB name
+    TABLE_NAME: process.env.TABLE_NAME || 'project001', // DynamoDB name
     BUCKET_NAME: process.env.BUCKET_NAME || 'project001',  // S3 bucket name  
     CHUNK_SIZE: process.env.CHUNK_SIZE || 10, // max simulateonos records in database
     ADDRESS: this.ENV === "PROD" ? "0.0.0.0" : "127.0.0.1",
